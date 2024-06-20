4c6a5b017e57744d500b415b553dd279f37e6c04
Adson <adson23_@hotmail.com>
2024-06-20 15:57:24 -0300

    using another way to print the final sentence

--- challenge4.py
+++ challenge4.py
@@ -1,4 +1,6 @@
 user_name = input('Qual o seu nome? ')
 age = int(input('Qual a sua idade? '))
 
-print('Olá, {}! Você tem {} anos.'.format(user_name, age))
+print('Olá, {}! Você tem {} anos.'.format(user_name, age)) # using format
+
+print(f'Olá, {user_name}! Você tem {age} anos.') # usin f string
