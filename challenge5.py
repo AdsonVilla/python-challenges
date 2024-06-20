c50268d6d8212d7e1faa033cfa7a5196972fdb73
Adson <adson23_@hotmail.com>
2024-06-20 16:21:53 -0300

    Challenge 5: working with math operations

---
+++ challenge5.py
@@ -0,0 +1,14 @@
+first_number = float(input('Please, type a number: '))
+second_number = float(input('Please, type another number: '))
+
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+potentiation = first_number ** second_number
+
+print(f'A soma entre eles é: {sum}')
+print(f'A subtração entre eles é: {subtraction}')
+print(f'A multiplicação entre eles é: {multiplication}')
+print(f'A divisão entre eles é: {division}')
+print(f'O numero {first_number} elevado ao número {second_number} é: {potentiation}')
