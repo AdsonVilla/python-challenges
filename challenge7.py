7b87f2c5f12c8b89fa38978f6ecc4ef34167511a
Adson <adson23_@hotmail.com>
2024-06-20 16:46:33 -0300

    Challenge 7: positions in a array

---
+++ challenge7.py
@@ -0,0 +1,5 @@
+fruits_list = ['apple', 'banana', 'mango', 'grape']
+last_item = len(fruits_list) - 1
+
+print(fruits_list[0])
+print(fruits_list[last_item]) # ou fruits_list[-1] para pegar o último item
