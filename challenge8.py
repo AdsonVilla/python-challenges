5eff0af6fb672a9a66286b5a940f6532f913e867
Adson <adson23_@hotmail.com>
2024-06-21 12:38:44 -0300

    Challange 8: modify a item and add item in the and of the list

---
+++ challenge8.py
@@ -0,0 +1,5 @@
+fruits_list = ['apple', 'banana', 'mango', 'grape']
+
+fruits_list[1] = 'strawbarry' # modifying a item of the list
+fruits_list.append('pineapple') # add item in the end of the list
+print(fruits_list)
